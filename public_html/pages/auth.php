52db092475653961f139c7d46949f85db7aaceee
tahmid16122009-crypto <tahmid16122009@gmail.com>
2026-06-20 22:10:12 +0600

    Update auth.php

--- public_html/pages/auth.php
+++ public_html/pages/auth.php
@@ -1,1 +1,20 @@
-h
+<?php
+require_once __DIR__ . '/../config.php';
+require_once __DIR__ . '/../database.php';
+require_once __DIR__ . '/../auth.php';
+
+$name   = trim($_POST['name']   ?? '');
+$mobile = trim($_POST['mobile'] ?? '');
+$next   = trim($_POST['next']   ?? '/home');
+
+// Validate next URL
+if (!preg_match('/^\/[a-zA-Z0-9\/?=&_-]*$/', $next)) {
+    $next = '/home';
+}
+
+if ($name && $mobile) {
+    rz_login_user($name, $mobile);
+}
+
+header('Location: ' . $next);
+exit;
